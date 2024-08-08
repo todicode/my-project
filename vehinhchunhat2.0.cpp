57cebf081a68cb9717f8d6bea8f341859e51f708
todicode <nguyentrangiabao7100@gmail.com>
2024-08-08 19:59:41 +0700

    Add files via upload

---
+++ vehinhchunhat2.0.cpp
@@ -0,0 +1,83 @@
+//#include <iostream>
+//#include <cmath>
+//  *****
+//  ~****
+//  ~~***
+//  ~~~**
+//  ~~~~*
+//using namespace std;
+//int main(){
+//    int n; cout << "type n: "; cin >> n;
+//    for (int i=1;i<=n;i++){
+//        for (int j=1;j<=n;j++){
+//            if (i<=j)
+//                cout << "*";
+//            else cout << "~";
+//        }
+//        cout << endl;
+//    }
+//}
+
+
+//*~~~~
+//**~~~
+//***~~
+//****~
+//*****
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main(){
+//    int n; cout << "type n: "; cin >> n;
+//    for (int i=1;i<=n;i++){
+//        for (int j=1;j<=n;j++){
+//            if (j<=i)
+//                cout << "*";
+//           else cout << "~";
+//        }
+//       cout << endl;
+//}
+//}
+
+
+
+
+//*****
+//****~
+//***~~
+//**~~~
+//*~~~~
+
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main(){
+//    int n; cout << "type n: "; cin >> n;
+//for (int i=1;i<=n;i++){
+//       for (int j=1;j<=n;j++){
+//            if (j<=n+1-i)
+//            cout << "*";
+//       else cout << "~";
+//    }
+//    cout << endl;
+//    }
+//}
+
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main(){
+//    int n; cout << "type n: ";cin>> n;
+//    for (int i=1;i<=n;i++){
+//        for (int j=1;j<=n;j++){
+//            if (j<=n-i){
+//                cout << "~";
+//        } else{
+//            if (j==n-i+1 || j==n || i==1 || i==n)
+//                cout << "*";
+//            else cout << " ";
+//    }
+//}
+//cout << endl;
+//}
+//}
